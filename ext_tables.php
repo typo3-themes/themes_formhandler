eda2dc028812b3ed6b499091416cff707a0326d3
Thomas Deuling <info@coding.ms>
2015-03-25 14:50:03 +0100

    [TASK] Dividing into different static templates

--- ext_tables.php
+++ ext_tables.php
@@ -1,2 +1,8 @@
 <?php
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Theme-Bootstrap - Formhandler');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Themes-Bootstrap - Formhandler (Base - required!)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/AppointmentRequest/', 'Themes-Bootstrap - Formhandler (AppointmentRequest)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactAdvanced/', 'Themes-Bootstrap - Formhandler (ContactAdvanced)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactAjaxBasic/', 'Themes-Bootstrap - Formhandler (ContactAjaxBasic)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactBasic/', 'Themes-Bootstrap - Formhandler (ContactBasic)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactMedium/', 'Themes-Bootstrap - Formhandler (ContactMedium)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/EstateSearch/', 'Themes-Bootstrap - Formhandler (EstateSearch)');
