b0f3473382d2721ac6336b75a961e4cf59b6b60b
Thomas Deuling <info@coding.ms>
2015-06-12 09:48:58 +0200

    [TASK] Integration of an advanced AJAX contact form

--- ChangeLog.md
+++ ChangeLog.md
@@ -3,6 +3,7 @@
 ### 2015-05-27  Thomas Deuling  <typo3@coding.ms>
 
 * [TASK] Adding PageTS by default, but only with pid-in-rootline condition
+* [TASK] Separation of master-template path and template path constants. Attention: themes.framework constant is now required!
 
 ### 2015-03-29  Thomas Deuling  <typo3@coding.ms>
 
--- ext_tables.php
+++ ext_tables.php
@@ -1,9 +1,10 @@
 <?php
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Themes - Formhandler (Base - required!)');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/AppointmentRequest/', 'Themes - Formhandler (AppointmentRequest)');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactAdvanced/', 'Themes - Formhandler (ContactAdvanced)');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactAjaxBasic/', 'Themes - Formhandler (ContactAjaxBasic)');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactBasic/', 'Themes - Formhandler (ContactBasic)');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactMedium/', 'Themes - Formhandler (ContactMedium)');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/EstateSearch/', 'Themes - Formhandler (EstateSearch)');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/MasterTemplateTest/', 'Themes - Formhandler (MasterTemplateTest)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Themes-Formhandler (Base - required!)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/AppointmentRequest/', 'Themes-Formhandler (AppointmentRequest)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactAdvanced/', 'Themes-Formhandler (ContactAdvanced)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactAjaxAdvanced/', 'Themes-Formhandler (ContactAjaxAdvanced)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactAjaxBasic/', 'Themes-Formhandler (ContactAjaxBasic)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactBasic/', 'Themes-Formhandler (ContactBasic)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactMedium/', 'Themes-Formhandler (ContactMedium)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/EstateSearch/', 'Themes-Formhandler (EstateSearch)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/MasterTemplateTest/', 'Themes-Formhandler (MasterTemplateTest)');
