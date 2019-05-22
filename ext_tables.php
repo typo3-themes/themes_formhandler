9463ad4c4ff8561974430710b6857bf653016f82
Thomas Deuling <info@coding.ms>
2019-05-22 07:57:53 +0200

    [FEATURE] Add page module/type for configuring different label.

--- ext_tables.php
+++ ext_tables.php
@@ -8,3 +8,14 @@
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/ContactMedium/', 'Themes-Formhandler (ContactMedium)');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/EstateSearch/', 'Themes-Formhandler (EstateSearch)');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/MasterTemplateTest/', 'Themes-Formhandler (MasterTemplateTest)');
+
+if (TYPO3_MODE === 'BE') {
+	
+	$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = array(
+		0 => 'Contact-Form Subjects', //LLL:EXT:news/Resources/Private/Language/locallang_be.xlf:news-folder',
+		1 => 'tfsubject',
+		2 => '../typo3conf/ext/themes_formhandler/Resources/Public/Icons/contactFormSubject.png'
+	);
+	\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon('pages', 'contains-tfsubject', '../typo3conf/ext/themes_formhandler/Resources/Public/Icons/contactFormSubject.png');
+	
+}
